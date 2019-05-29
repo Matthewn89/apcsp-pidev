eb6d63c5358d0e4b223c2e798a04c34879797bfc
Matthewn89 <50209676+Matthewn89@users.noreply.github.com>
2019-05-29 09:04:21 -0700

    Create student.c

---
+++ cdemo/myproject/student.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "student.h"
+
+void printStudent(struct Student* student) {
+  printf("Student:\n");
+  printf("Name: %s %s\n",student -> firstName, student -> lastName);
+  printf("Age: %d\n", student -> age);
+  printf("Student ID: %d\n", student -> studentID);
+}
